6549574414e5d16cf8e6e0ed2a328f6641a8889c
Jose Hurtado <josehumberto17@gmail.com>
2020-04-19 07:11:51 -0500

    tests get and post for api redmine

---
+++ src/test/java/com/company/projectrestassured/features/Test1.java
@@ -0,0 +1,94 @@
+package com.company.projectrestassured.features;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.given;
+
+public class Test1 {
+
+    private static RequestSpecification request;
+    private static Response response;
+
+
+    @Test
+    public void requestGetProjectsRedmine_checkResponseCode_expect200_and_total_count10() {
+
+        //Codigo Http Respuesta
+        //Codigo del servicio satisfactorio
+        //Esquema del servicio
+        //El contenido de la respuesta del servicio
+
+        request = given()
+                .contentType("application/json");
+
+        response = request.when()
+                .get("http://192.168.2.110:8081/projects.json");
+
+
+        response.then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(200);
+
+        JsonPath jsonPath = new JsonPath(response.getBody().asString());
+
+        Assertions.assertEquals(10, jsonPath.getInt("total_count"));
+
+    }
+
+
+    @Test
+    public void requestGetIssuesRedmine_checkResponseCode_expect200() {
+
+        request = given()
+                .contentType("application/json");
+
+        response = request
+                .when()
+                .get("http://192.168.2.110:8081/issues.json");
+
+        response.then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(200);
+
+        JsonPath jsonPath = new JsonPath(response.getBody().asString());
+
+        Assertions.assertEquals(2, jsonPath.getInt("total_count"));
+
+
+    }
+
+    @Test
+    public void  requestPostCreateAIssueRedmine_checkResponseCode_expect201(){
+        request = given()
+                .contentType("application/json");
+
+        response = request
+                .when()
+                .header("X-Redmine-API-Key", "475338c5e8fe95ef9f9c1cae8cb69610e2d413ac")
+                .body("{\n" +
+                        "  \"issue\": {\n" +
+                        "\t  \"project_id\": 1,\n" +
+                        "\t  \"subject\": \"Mi segundo issue creado desde RestAssured\",\n" +
+                        "\t  \"description\": \"As an admin user, I cannot create an user when...\",\n" +
+                        "\t  \"status_id\": 1,\n" +
+                        "\t  \"priority_id\": 1\n" +
+                        "\t}\n" +
+                        "}")
+                .post("http://192.168.2.110:8081/issues.json");
+
+        response.then()
+                .log()
+                .all()
+                .assertThat()
+                .statusCode(201);
+    }
+}
