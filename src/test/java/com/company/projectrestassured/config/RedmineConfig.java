4b338fa085b0a05fc81a41e9d11b35b5dd608607
Jose Hurtado <josehumberto17@gmail.com>
2020-05-02 06:20:33 -0500

    Update to junit jupiter

--- src/test/java/com/company/projectrestassured/config/RedmineConfig.java
+++ src/test/java/com/company/projectrestassured/config/RedmineConfig.java
@@ -7,36 +7,38 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-
-import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
 public class RedmineConfig {
 
-    public static RequestSpecification requestSpecification;
+    private static RequestSpecification requestSpecification;
 
-    public static ResponseSpecification responseSpecification;
+    private static ResponseSpecification responseSpecification;
 
-
-    @BeforeClass
+    @BeforeAll
     public static void setup(){
 
         requestSpecification = new RequestSpecBuilder()
-                        .setBaseUri("http://45.55.55.186")
+                        .setBaseUri("http://localhost")
                         .setBasePath("/")
                         .setPort(8081)
                         .addHeader("Content-Type", "application/json")
                         .addHeader("Accept", "application/json")
+                        .addHeader("X-Redmine-API-Key","3bf031106d2a0f3878e225d012627b995f5eeb9d")
                         .addFilter(new RequestLoggingFilter())
                         .addFilter(new ResponseLoggingFilter())
-                        .addHeader("X-Redmine-API-Key","c2481248780185b6944b74fb8a2ca8835cd35ab7")
                         .build();
 
         responseSpecification = new ResponseSpecBuilder()
-                        //.expectStatusCode(200)
                         .build();
-
 
         RestAssured.requestSpecification = requestSpecification;
         RestAssured.responseSpecification = responseSpecification;
+    }
+
+    @AfterAll
+    public static void cleanUp(){
+        RestAssured.reset();
     }
 }
